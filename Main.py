6026726afca9e7487946269ed0aa6b48130e9c02
MrNoobPy <tarjomebama@gmail.com>
2024-07-17 19:07:02 +0200

    FastAPI: adding parameters, path and query

--- Main.py
+++ Main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 from enum import Enum
 from typing import Optional
-
-
-# import uvicorn
+import uvicorn
 
 app = FastAPI()
 
 
+@app.get('/home/{name}/{age}')
+def home(name: str, age: int, postcode: Optional[str] = None, validity: bool = True):
+    return {'name': f'{name}',
+            'age': f'{age}',
+            'postcode': f'{postcode}',
+            'validity': f'{validity}'
+            }
