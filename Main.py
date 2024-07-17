6a7a76f5d984a2254bc875c8d8ac2f8848775fc2
MrNoobPy <tarjomebama@gmail.com>
2024-07-17 23:33:09 +0200

    FastAPI: Importing router and organizing tasks

--- Main.py
+++ Main.py
@@ -2,39 +2,13 @@ from fastapi import FastAPI, status, Response
 from enum import Enum
 from typing import Optional
 import uvicorn
+from router import myhome_get
 
 app = FastAPI()
 
-
-@app.get('/myhome', summary='Home page.', description='We are learning FastAPI and this is an example description.')
-def home():
-    return 'home page'
+app.include_router(myhome_get.router)
 
 
-@app.get('/myhome/pageone/{name}/{age}', tags=['learning'])
-def my_details(name: str, age: int, postcode: Optional[str] = None, validity: bool = True):
-    return {'name': f'{name}',
-            'age': f'{age}',
-            'postcode': f'{postcode}',
-            'validity': f'{validity}'
-            }
-
-
-@app.get('/myhome/pagetwo', status_code=status.HTTP_200_OK, tags=['learning'], response_description='This is related to 200 ok')
-def my_status(ids: int, response: Response):
-    """
-    - **ids**: This is where I can explain what id is
-    """
-    if ids > 5:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return 'id too big'
-    return 'id is ok'
-
-
-@app.get('/myhome/pagethree', tags=['Practice'], response_description='It is a response description.')
-def to_repeat():
-    """
-    **This is also a description written in the method.**
-    """
-    return "ok"
-
+@app.get('/')
+def home_page():
+    return 'This the home page with no endpoint'
--- router/myhome_get.py
+++ router/myhome_get.py
@@ -1,3 +1,42 @@
 from fastapi import APIRouter
+from fastapi import FastAPI, status, Response
+# from enum import Enum
+from typing import Optional
+
 
 router = APIRouter(prefix='/myhome', tags=['myhome'])
+
+
+@router.get('/myhome', summary='Home page.',
+            description='We are learning FastAPI and this is an example description.'
+            )
+def home():
+    return 'home page'
+
+
+@router.get('/myhome/pageone/{name}/{age}')
+def my_details(name: str, age: int, postcode: Optional[str] = None, validity: bool = True):
+    return {'name': f'{name}',
+            'age': f'{age}',
+            'postcode': f'{postcode}',
+            'validity': f'{validity}'
+            }
+
+
+@router.get('/myhome/pagetwo', status_code=status.HTTP_200_OK, response_description='This is related to 200 ok')
+def my_status(ids: int, response: Response):
+    """
+    - **ids**: This is where I can explain what ids is
+    """
+    if ids > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return 'id too big'
+    return 'id is ok'
+
+
+@router.get('/myhome/pagethree', tags=['Practice'], response_description='It is a response description.')
+def to_repeat():
+    """
+    **This is also a description written in the method.**
+    """
+    return "ok"
