0e8ffa5c6f7fbfb3a807906bdccae09b974921d9
MrNoobPy <tarjomebama@gmail.com>
2024-07-17 18:37:15 +0200

    FastAPI: removing everything to organize and practice

--- Main.py
+++ Main.py
@@ -8,31 +8,3 @@ from typing import Optional
 app = FastAPI()
 
 
-@app.get('/')
-def hello():
-    return 'home page nothing after slash'
-
-
-@app.get('/log-in')
-def log_in():
-    return 'using log in method with a get'
-
-
-@app.get('/blogs/{name}')
-def blog_name(name: int):
-    return {'message': f'{name} is taken from user after blogs'}
-
-
-class Limiter(str, Enum):
-    Mesal1 = 'One'
-    Mesal2 = 'Two'
-
-
-@app.get('/queries')
-def test_query(name: str = None, age: int = None):
-    return {'message': f'{name=} -- {age=}'}
-
-
-@app.get('/queriiestwo')
-def test_quereis_two(country: Optional[int] = None):
-    return {'message': f'{country}'}
