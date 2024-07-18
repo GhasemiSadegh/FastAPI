c650b414fe6d29b307e2c10501c6af0ff27d4b15
MrNoobPy <tarjomebama@gmail.com>
2024-07-18 19:01:52 +0200

    FastAPI: Creating BaseModel for myhome

--- router/myhome_post.py
+++ router/myhome_post.py
@@ -1,8 +1,16 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter(prefix='/blog', tags=['posts'])
 
 
+class PydanticValidator(BaseModel):
+    name: str
+    age: int
+    postalcode: int
+    street: str
+
+
 @router.post('/new')
-def my_post():
-    return {'Jason': 'ok'}
+def my_post(myhome: BaseModel):
+    return {'Msg': 'ok', "data": myhome}
