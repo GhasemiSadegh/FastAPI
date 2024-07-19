92996894b5513a7c3caa694a88fdd2f3c2097a15
MrNoobPy <tarjomebama@gmail.com>
2024-07-19 11:59:41 +0200

    FastAPI: Creating a Query with title, description, alias, deprecated.

--- router/myhome_post.py
+++ router/myhome_post.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from pydantic import BaseModel
 from typing import Optional
 
@@ -12,8 +12,23 @@ class PydanticValidator(BaseModel):
     present: bool = True
 
 
-@router.post('/posts')
-def my_post(myhome: PydanticValidator):
-    print(myhome.name)
-    print(myhome.age)
-    return {'Msg': 'ok', "data": myhome}
+@router.post('/users')
+def my_users(data: PydanticValidator):
+    return {'Msg': 'ok', "data": data} # this will go back to client/user = response body
+
+
+@router.post('/users/details/{id}')
+def my_details(data: PydanticValidator, id: int, version: int = 1):
+    return {'msg': 'Your are registered.',
+            'data': data,
+            'Your id': id,
+            'The version': version}
+
+
+@router.post('/users/questions')
+def my_questions(data: PydanticValidator, question_id: int = Query(None, title='text', description='text',
+                                                                   alias='QuestionID', deprecated=False)):
+    return {
+        'data': data,
+        'question_id': question_id
+    }
