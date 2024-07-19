b87edb6354fb86074ee2b64f2ab53ac6e6a3d930
MrNoobPy <tarjomebama@gmail.com>
2024-07-19 13:23:16 +0200

    FastAPI: Creating search for the library

--- router/practice.py
+++ router/practice.py
@@ -2,9 +2,16 @@ from fastapi import FastAPI, APIRouter
 
 router = APIRouter()
 
+class
+
 
 @router.get('/my-library/books/{book_id}') # book_id is embedded in the link where the book photo is
 def photos(book_id):
     if book_id:
         return 'Available'
     return 'Not available'
+
+
+@router.get('my-library/books/search')
+def search(author: str, genre: str, year: int, name, str):
+
