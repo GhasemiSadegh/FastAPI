7b53012900f50f5d622c9909c95c475738494a68
MrNoobPy <tarjomebama@gmail.com>
2024-07-26 20:39:46 +0200

    FastAPI_SQLAlchemy: Creating a bands page and list of bands

--- BasicsEnglish/mymain.py
+++ BasicsEnglish/mymain.py
@@ -9,5 +9,19 @@ async def index():
 
 
 @app.get('/about')
-async def about():
+async def about() -> str:
     return 'An amazing company'
+
+BANDS = [
+    {'id': 1, 'name': 'Ali', 'genre': 'Rock'},
+    {'id': 2, 'name': 'Reza', 'genre': 'Jazz'},
+    {'id': 3, 'name': 'Gholam', 'genre': 'Hip Hop'},
+    {'id': 4, 'name': 'Feri', 'genre': 'Pop'}
+]
+
+
+@app.get('/bands')
+async def bands() -> list[dict]:
+    return BANDS
+
+
