09ed90fbd00b2e2f167edf868160e4144ae203ae
MrNoobPy <tarjomebama@gmail.com>
2024-07-26 19:50:04 +0200

    FastAPI_SQLAlchemy: Giving new port and using async

---
+++ BasicsEnglish/mymain.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+async def index():
+    return {'message': "Hello World"}
+
+
+@app.get('/about')
+async def about():
+    return 'An amazing company'
