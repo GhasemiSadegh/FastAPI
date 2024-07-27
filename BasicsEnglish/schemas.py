81872f49159fe5c126cce13ed4a5cd7a0e0a3ed1
MrNoobPy <tarjomebama@gmail.com>
2024-07-27 12:16:29 +0200

    FastAPI_SQLAlchemy: Adding albums

--- BasicsEnglish/schemas.py
+++ BasicsEnglish/schemas.py
@@ -1,11 +1,21 @@
 from enum import Enum
 from pydantic import BaseModel
+from datetime import date
+
+
+class Album(BaseModel):
+    title: str
+    release_date: date
 
 
 class Band(BaseModel):
     id: int
     name: str
     genre: str
+    albums: list[Album] = []
+
+
+
 
 
 class GenreURLChoices(Enum):
