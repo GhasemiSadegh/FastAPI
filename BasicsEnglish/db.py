543804ca89c6496235e1292c0cff433707bd6635
MrNoobPy <tarjomebama@gmail.com>
2024-07-31 22:09:13 +0200

    FastAPI: Creating a function to inject the session

--- BasicsEnglish/db.py
+++ BasicsEnglish/db.py
@@ -6,3 +6,8 @@ engine = create_engine(DATABASE_URL, echo=True)
 
 def init_db():
     SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:  # with closes the session closes properly
+        yield session  # yields the session for use in operations
