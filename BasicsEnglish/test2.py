042b3bacef0aa6c2b1e133438080b9ba83f85eb2
MrNoobPy <tarjomebama@gmail.com>
2024-07-31 12:58:10 +0200

    FastAPI: Creating another decorator as practice

--- BasicsEnglish/test2.py
+++ BasicsEnglish/test2.py
@@ -1,1 +1,17 @@
+def decorator(func):
+
+    def wrapper(*args, **kwargs):
+        print('this is your information')
+        main = func(*args, **kwargs)
+        print('see you next time')
+
+        return main
+    return wrapper()
+
+
+@decorator
+def info():
+    print('x')
+
+
 
