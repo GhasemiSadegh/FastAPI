d7fc0b87d7fa857b5a6945dd10b0207580682579
MrNoobPy <tarjomebama@gmail.com>
2024-07-31 10:58:38 +0200

    FastAPI: Writing a decorator

--- BasicsEnglish/test.py
+++ BasicsEnglish/test.py
@@ -1,5 +1,13 @@
-from typing import Annotated
+# from typing import Annotated
+#
+#
+# def sums(a: Annotated[int, (0, 10)], b: int):
+#     return a + b
 
-
-def sums(a: Annotated[int, (0, 10)], b: int):
-    return a + b
+def timer(f):
+    def wrapper(*args, **kwargs):
+        print('Start')
+        main = f(*args, **kwargs)
+        print('End')
+        return main
+    return wrapper()
--- db.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import Session, create_engine, SQLModel
-
-DATABASE_URL = 'sqlite:///db.sqlite'
-engine = create_engine(DATABASE_URL)
-from fastapi import FastAPI
-
-
-def init_db():
-    SQLModel.metadata.create_all(engine)
