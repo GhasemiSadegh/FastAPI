2bb8c71705fdbc5fd48837573c30ac18365e744b
MrNoobPy <tarjomebama@gmail.com>
2024-07-31 20:16:41 +0200

    FastAPI: Learning Depends in fastapi

--- BasicsEnglish/test2.py
+++ BasicsEnglish/test2.py
@@ -1,39 +1,68 @@
-# def decorator(func):
+# # def decorator(func):
+# #
+# #     def wrapper(*args, **kwargs):
+# #         print('this is your information')
+# #         main = func(*args, **kwargs)
+# #         print('see you next time')
+# #
+# #         return main
+# #     return wrapper()
+# #
+# #
+# # @decorator
+# # def info():
+# #     print('x')
+# #
+# #
+# #
 #
+# def timer(f):
 #     def wrapper(*args, **kwargs):
-#         print('this is your information')
-#         main = func(*args, **kwargs)
-#         print('see you next time')
-#
+#         print('Start')
+#         main = f(*args, **kwargs)
+#         print('End')
 #         return main
-#     return wrapper()
+#
+#     return wrapper
 #
 #
-# @decorator
-# def info():
-#     print('x')
+# @timer
+# def adder(a, b):
+#     print(a + b)
+#
+#
+# adder(1, 2)
 #
 #
 #
+#
+#
+#
+# def count_up_to(n):
+#     count = 1
+#     while count <= n:
+#         yield count
+#         count += 1
+#
+#
+# my_count = count_up_to(10)
+#
+# print(next(my_count))
+# print(next(my_count))
+# print(next(my_count))
+# print(next(my_count))
 
-def timer(f):
-    def wrapper(*args, **kwargs):
-        print('Start')
-        main = f(*args, **kwargs)
-        print('End')
-        return main
+from fastapi import FastAPI, Depends
 
-    return wrapper
+app = FastAPI()
 
 
-@timer
-def adder(a, b):
-    print(a + b)
+def function():
+    x = 1
+    return x + 1
 
 
-adder(1, 2)
-
-
-
-
+@app.get("/items/")
+async def vabaste(y: int = Depends(function)):
+    return {"result": y + 1}
 
