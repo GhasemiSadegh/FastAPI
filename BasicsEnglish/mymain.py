93388e759b32ade1458b1df983ff85b30800f465
MrNoobPy <tarjomebama@gmail.com>
2024-08-01 21:29:26 +0200

    FastAPI: Updating get and post; table created. Checking data in DBeavers

--- BasicsEnglish/mymain.py
+++ BasicsEnglish/mymain.py
@@ -3,7 +3,7 @@ from models import GenreURLChoices, BandBase, BandCreate, Band, Album
 from typing import Annotated
 from contextlib import asynccontextmanager
 from db import init_db, get_session
-from sqlmodel import Session
+from sqlmodel import Session, select
 
 
 @asynccontextmanager
@@ -15,56 +15,32 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-#
-# @app.get('/')
-# async def index():
-#     return {'message': "Hello World"}
-#
-#
-# @app.get('/about')
-# async def about() -> str:
-#     return 'An amazing company'
-#
-#
-# BANDS = [
-#     {'id': 1, 'name': 'Ali', 'genre': 'Rock',
-#      'albums': [
-#          {'title': 'Flowers of Spring', 'release_date': '2023-01-02'}
-#      ]
-#      },
-#     {'id': 2, 'name': 'Reza', 'genre': 'Jazz'},
-#     {'id': 3, 'name': 'Gholam', 'genre': 'Hip Hop',
-#      'albums': [
-#          {'title': 'Master of Reality', 'release_date': '2024-01-01'}
-#      ]
-#      },
-#     {'id': 4, 'name': 'Feri', 'genre': 'Pop'}
-# ]
-#
-#
-# @app.get('/bands')
-# async def bands(genre: GenreURLChoices | None = None,
-#                 q: Annotated[str | None, Query(max_length=10)] = None) -> list[Band]:
-#     band_list = [Band(**b) for b in BANDS]  # ** unpacks the values of each item in the dict
-#
-#     if q:
-#         band_list = [b for b in band_list if q.lower() in b.name.lower()
-#                      ]
-#     if genre:
-#         band_list = [b for b in band_list if genre.value == b.genre.lower()
-#                      ]
-#     # if has_album:
-#     #     band_list = [b for b in band_list if len(b.albums) > 0]
-#
-#     return band_list
-#
-#
-# @app.get('/bands/{band_id}')
-# async def band(band_id: Annotated[int, Path(title="Band ID", description='This is description')]) -> Band:
-#     band = next((Band(**b) for b in BANDS if b['id'] == band_id), None)
-#     if band is None:
-#         raise HTTPException(status_code=404, detail='band not found')
-#     return band
+@app.get('/bands')
+async def bands(
+        genre: GenreURLChoices | None = None,
+        q: Annotated[str | None, Query(max_length=10)] = None,
+        session: Session = Depends(get_session)
+) -> list[Band]:
+
+    band_list = session.exec(select(Band)).all()
+    if q:
+        band_list = [b for b in band_list if q.lower() in b.name.lower()
+                     ]
+    if genre:
+        band_list = [b for b in band_list if genre.value == b.genre.lower()
+                     ]
+    return band_list
+
+
+@app.get('/bands/{band_id}')
+async def band(
+        band_id: Annotated[int, Path(title="Band ID", description='This is description')],
+        session : Session = Depends(get_session)
+) -> Band:
+    band = session.get(Band, band_id)
+    if band is None:
+        raise HTTPException(status_code=404, detail='band not found')
+    return band
 #
 #
 # # @app.get('/bands/genre/{genre}')
