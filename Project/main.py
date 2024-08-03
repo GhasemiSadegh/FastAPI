36f0823ce7100494e53348ceb9361662a6bc1751
MrNoobPy <tarjomebama@gmail.com>
2024-08-03 10:27:17 +0200

    FastAPI: Creating a database, post method and testing Swagger UI

--- Project/main.py
+++ Project/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from database import Library
+from database import Library, Session, engine
 
 
 app = FastAPI()
@@ -7,4 +7,6 @@ app = FastAPI()
 
 @app.post('/library')
 def create_book(book: Library):
-    return 'ok'
+    with Session(engine) as session:
+        session.add(book)
+        session.commit()
