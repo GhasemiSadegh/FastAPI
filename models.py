2bfadb1c0f73c7bcc32a2981ded252aa377fd995
MrNoobPy <tarjomebama@gmail.com>
2024-08-04 15:26:52 +0200

    FastAPI: Creating 3 versions of database by adding and removing columns in the table

--- models.py
+++ models.py
@@ -2,10 +2,9 @@ from sqlmodel import SQLModel, Field
 # from datetime import date
 
 
-class BaseLibrary(SQLModel, table=True):
+class BaseLibrary(SQLModel):
     id: int = Field(default=None, primary_key=True)
     title: str = Field(default=None, max_length=20, unique=True)
     author: str = Field(default=None, min_length=3)
     pub_year: int = Field(default=None, description='The year the book was published.')
     genre: str = Field(default=None, title='Genres')
-    pages: int
